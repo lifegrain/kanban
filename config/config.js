58fecae626be3d3e4bdf8ca761f5dd18b66d1b9c
lifegrain <terrykho@ymail.com>
2020-03-13 17:52:06 +0700

    Update config.js

--- config/config.js
+++ config/config.js
@@ -13,13 +13,13 @@ module.exports = {
     "password": null,
     "database": "database_test",
     "host": "127.0.0.1",
-    "dialect": "mysql"
+    "dialect": "postgres"
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+    "username": `smurvkwolzqlqt`,
+    "password": `006a4f678d6f59755b2ea77e6527bd2ae8900a47d2925c88066e94339d3d6d26`,
+    "database": "dbiocskp5veeoe",
+    "host": "ec2-50-17-178-87.compute-1.amazonaws.com",
+    "dialect": "postgres"
   }
 }
